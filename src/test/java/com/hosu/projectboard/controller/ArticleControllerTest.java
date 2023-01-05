ecdbd03d653f7e6362bbd1d8dec2af5bec427eb5
hosu <hojune0904@gmail.com>
2023-01-05 17:11:13 +0900

    #17 - 로그인 컨트롤러 테스트, + 시큐리티 설정을 컨트롤러 테스트 반영

--- src/test/java/com/hosu/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/hosu/projectboard/controller/ArticleControllerTest.java
@@ -1,20 +1,21 @@
 package com.hosu.projectboard.controller;
 
+import com.hosu.projectboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @DisplayName("View 컨트롤러 - 게시판")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
