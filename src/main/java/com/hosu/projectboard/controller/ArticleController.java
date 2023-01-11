13d08d017430da13ba3452b7bf32bdec67f2650a
hosu <hojune0904@gmail.com>
2023-01-11 19:15:48 +0900

    #42 - 시큐리티 설정과 자바 대응 코드 구현

--- src/main/java/com/hosu/projectboard/controller/ArticleController.java
+++ src/main/java/com/hosu/projectboard/controller/ArticleController.java
@@ -2,8 +2,8 @@ package com.hosu.projectboard.controller;
 
 import com.hosu.projectboard.domain.constant.FormStatus;
 import com.hosu.projectboard.domain.constant.SearchType;
-import com.hosu.projectboard.dto.UserAccountDto;
 import com.hosu.projectboard.dto.request.ArticleRequest;
+import com.hosu.projectboard.dto.security.BoardPrincipal;
 import com.hosu.projectboard.response.ArticleResponse;
 import com.hosu.projectboard.response.ArticleWithCommentsResponse;
 import com.hosu.projectboard.service.ArticleService;
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,7 @@ import java.util.List;
 public class ArticleController {
     private final ArticleService articleService;
     private final PaginationService paginationService;
+
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
@@ -39,6 +41,7 @@ public class ArticleController {
         map.addAttribute("searchTypes", SearchType.values());
         return "articles/index";
     }
+
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticleWithComments(articleId));
@@ -47,6 +50,7 @@ public class ArticleController {
         map.addAttribute("totalCount", articleService.getArticleCount());
         return "articles/detail";
     }
+
     @GetMapping("/search-hashtag")
     public String searchArticleHashtag(
             @RequestParam(required = false) String searchValue,
@@ -62,37 +66,44 @@ public class ArticleController {
         map.addAttribute("searchType", SearchType.HASHTAG);
         return "articles/search-hashtag";
     }
+
     @GetMapping("/form")
     public String articleForm(ModelMap map) {
         map.addAttribute("formStatus", FormStatus.CREATE);
         return "articles/form";
     }
+
     @PostMapping("/form")
-    public String postNewArticle(ArticleRequest articleRequest) {
-        // TODO: 인증 정보를 넣어줘야 한다.
-        articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
-        )));
+    public String postNewArticle(
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+            ArticleRequest articleRequest
+    ) {
+        articleService.saveArticle(articleRequest.toDto(boardPrincipal.toDto()));
 
         return "redirect:/articles";
     }
+
     @GetMapping("/{articleId}/form")
-    public String updateArticleForm(@PathVariable Long articleId, ModelMap map) {
-        ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
-        map.addAttribute("article", article);
-        map.addAttribute("formStatus", FormStatus.UPDATE);
+    public String updateArticle(
+            @PathVariable Long articleId,
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+            ArticleRequest articleRequest
+    ) {
+        articleService.updateArticle(articleId, articleRequest.toDto(boardPrincipal.toDto()));
         return "articles/form";
     }
-    @PostMapping ("/{articleId}/form")
-    public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest) {
-        // TODO: 인증 정보를 넣어줘야 한다.
-        articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
-                "uno", "asdf1234", "uno@mail.com", "Uno", "memo"
-        )));
+
+    @PostMapping("/{articleId}/form")
+    public String deleteArticle(
+            @PathVariable Long articleId,
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal
+    ) {
+        articleService.deleteArticle(articleId, boardPrincipal.getUsername());
 
         return "redirect:/articles/" + articleId;
     }
-    @PostMapping ("/{articleId}/delete")
+
+    @PostMapping("/{articleId}/delete")
     public String deleteArticle(@PathVariable Long articleId) {
         // TODO: 인증 정보를 넣어줘야 한다.
         articleService.deleteArticle(articleId);
